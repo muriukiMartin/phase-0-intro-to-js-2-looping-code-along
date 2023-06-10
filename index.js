41672d50c66965db66eab67f658f9aa78f756261
helheim254@gmail.com <MM@Reginas-MacBook-Air.local>
2023-06-10 12:35:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for(let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+function countDown(n) {
+    let i = 0;
+    while (i <= n) {
+        console.log(n--);
+    }
+}
